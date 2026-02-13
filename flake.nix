c14ed55a261ffedfeaca9098e1fcdc288cc96ab5
ToasterUwU <Aki@ToasterUwU.com>
2026-02-13 18:04:36 +0100

    use the same nixpkgs version for the niri flake

--- flake.nix
+++ flake.nix
@@ -37,7 +37,10 @@
       url = "github:powerofthe69/nix-gaming-edge";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    niri.url = "github:sodiboo/niri-flake";
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     elephant.url = "github:abenz1267/elephant";
     walker = {
       url = "github:abenz1267/walker";
