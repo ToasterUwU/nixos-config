f8e0b0425685dc8d8bbc3b11efbf0d40994dfd9c
ToasterUwU <Aki@ToasterUwU.com>
2026-03-09 04:48:56 +0100

    Use ZSwap instead of ZRam, at least for testing for now

--- common/configuration.nix
+++ common/configuration.nix
@@ -15,7 +15,14 @@
   nix.settings.trusted-users = [ "aki" ];
 
   # boot.kernelPackages = pkgs.linuxPackages_zen;
-  zramSwap.enable = true;
+
+  boot.initrd.systemd.enable = true;
+  boot.kernelParams = [
+    "zswap.enabled=1" # enables zswap
+    "zswap.compressor=lz4" # compression algorithm
+    "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+  ];
 
   programs.nix-ld.enable = true;
   programs.appimage = {
