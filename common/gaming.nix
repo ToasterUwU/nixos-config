81d88c9937298df129f9fcf3f492dfc7ed9e378a
ToasterUwU <Aki@ToasterUwU.com>
2026-08-25 22:23:00 +0200

    i plan to use sunshine as a remote play together alternative, so an autostart is just a security risk for me

--- common/gaming.nix
+++ common/gaming.nix
@@ -31,7 +31,6 @@
 
   services.sunshine = {
     enable = true;
-    autoStart = true;
     capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
     openFirewall = true;
   };
