4871740d81860c8b18dc18980d5902a970ec5999
ToasterUwU <Aki@ToasterUwU.com>
2026-09-12 21:29:42 +0200

    update tirith to fix it failing under NixOS and refusing any shell commands outside of a FSH

--- flake.nix
+++ flake.nix
@@ -40,6 +40,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/548099.diff";
       flake = false;
     };
+    nixpkgs-patch-fix-tirith = {
+      url = "https://github.com/NixOS/nixpkgs/pull/562326.diff";
+      flake = false;
+    };
   };
 
   outputs =
